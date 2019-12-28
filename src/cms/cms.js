1d4b7698673c06befb55dddb7f7a7f2e186abc4f
Steve Wisner <spwisner@gmail.com>
2019-12-28 14:43:14 -0500

    Add resume page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,7 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import ScriptsPagePreview from './preview-templates/ScriptsPagePreview'
+import ResumePagePreview from './preview-templates/ResumePagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
@@ -12,5 +13,6 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('scripts', ScriptsPagePreview)
+CMS.registerPreviewTemplate('resume', ResumePagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,6 +59,9 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
+              <Link className="navbar-item" to="/resume">
+                Resume
+              </Link>
               <Link className="navbar-item" to="/scripts">
                 Scripts
               </Link>
