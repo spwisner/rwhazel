2d38390e5857b2eb2dd1564f88dc5fdff6a73500
Steve Wisner <spwisner@gmail.com>
2020-01-01 13:39:28 -0500

    Add imdb svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
-import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import imdb from '../img/social/imdb.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -36,8 +33,24 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
+                      <Link className="navbar-item" to="/blog">
+                        Blog
+                      </Link>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+              <div className="column is-4">
+                <section>
+                  <ul className="menu-list">
+                    <li>
                       <Link className="navbar-item" to="/interviews">
                         Interviews
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="navbar-item" to="/contact">
+                        Contact
                       </Link>
                     </li>
                     <li>
@@ -53,31 +66,15 @@ const Footer = class extends React.Component {
                   </ul>
                 </section>
               </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/RWH4POTUS/" target="_blank">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com/robertwhazel69" target="_blank">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -85,17 +82,11 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="imdb" href="https://www.imdb.com/name/nm7499519/" target="_blank">
                   <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
+                    className="fas fa-lg"
+                    src={imdb}
+                    alt="IMDb"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
