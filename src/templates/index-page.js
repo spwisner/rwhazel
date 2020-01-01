35e27953e3656f16793ed2919544dcd67428726b
Steve Wisner <spwisner@gmail.com>
2020-01-01 13:46:56 -0500

    Remove broken query fields

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -101,25 +101,7 @@ export const pageQuery = graphql`
         }
         heading
         subheading
-        mainpitch {
-          title
-          description
-        }
         description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
