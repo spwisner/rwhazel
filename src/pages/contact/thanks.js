62f554b0200c3cabfd1603b4ea1385998043504f
Steve Wisner <spwisner@gmail.com>
2020-01-02 20:23:48 -0500

    Update thank you page and styles

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -3,6 +3,17 @@ import Layout from '../../components/Layout'
 
 export default () => (
   <Layout>
+    <div className='generic-page-wrap contact-page'>
+      <div className="container">
+        <div className="content">
+          <div className='title-wrap contact-title'>
+            <h2 className="title is-size-3 has-text-weight-bold is-bold-light" style={{ marginBottom: '0.75rem', marginTop: '0.75rem' }}>
+              Contact
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
     <section className="section">
       <div className="container">
         <div className="content">
